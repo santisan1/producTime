252ea6400607c5cf164c5155fe2c57e4d4c83a6c
santisan1 <108411999+santisan1@users.noreply.github.com>
2026-03-17 09:12:35 -0300

    b

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 
 
-const SECRET_HASH = "BE61F00B0B3C1E4FFCF02370171677DB0856127544284AB41AAF7B01689E74D9";
+const SECRET_HASH = "831677c77f0d0143890252579294273f5507119f964a5728a47468132334812f";
 
 const renderApp = () => {
   createRoot(document.getElementById('root')).render(
@@ -17,31 +17,27 @@ const renderApp = () => {
 
 // 2. Lógica del Bouncer
 const bouncer = () => {
-  // Si estás trabajando en tu compu (localhost), no te pide nada
-  if (window.location.hostname === 'localhost') {
-    return renderApp();
-  }
+  if (window.location.hostname === 'localhost') return renderApp();
 
-  // Si ya pusiste la clave antes, te deja pasar
-  if (sessionStorage.getItem('auth_token') === SECRET_HASH) {
-    return renderApp();
-  }
+  const sessionAuth = sessionStorage.getItem('auth_token');
+  if (sessionAuth === SECRET_HASH) return renderApp();
 
-  // Si no, te pide la clave
-  const userPass = prompt("Acceso restringido. Ingresá la clave:");
+  const userPass = prompt("Ingresá la clave (TTE123):");
 
   if (userPass) {
-    const userHash = CryptoJS.SHA256(userPass).toString();
+    // .trim() borra espacios accidentales al principio o final
+    const cleanPass = userPass.trim();
+    const userHash = CryptoJS.SHA256(cleanPass).toString();
+
     if (userHash === SECRET_HASH) {
       sessionStorage.setItem('auth_token', userHash);
       renderApp();
     } else {
-      alert("Clave incorrecta.");
-      window.location.reload(); // Recarga para volver a preguntar
+      alert("Clave incorrecta. ¡Ojo con las mayúsculas!");
+      window.location.reload();
     }
   } else {
     document.body.innerHTML = "<h1 style='color:white;text-align:center;margin-top:20%'>Acceso Denegado</h1>";
   }
 };
-
 bouncer();
